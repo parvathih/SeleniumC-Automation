21d354bfec28f69a441c85808bc087c827828214
parvathih <parvathi.testing@gmail.com>
2024-05-18 03:07:59 +0530

    Adding Login and Basepage

---
+++ Pages/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+public class BasePage
+{
+    protected IWebDriver driver;
+
+    public BasePage(IWebDriver driver)
+    {
+        this.driver = driver;
+    }
+
+    protected void EnterText(By by, string text)
+    {
+        driver.FindElement(by).SendKeys(text);
+    }
+
+    protected void Click(By by)
+    {
+        driver.FindElement(by).Click();
+    }
+}
---
+++ Pages/LoginPage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+public class LoginPage : BasePage
+{
+    private readonly string URL = "https://practicetestautomation.com/practice-test-login/";
+
+    private readonly By USERNAME_FIELD = By.Id("username");
+    private readonly By PASSWORD_FIELD = By.Id("password");
+    private readonly By LOGIN_BUTTON = By.Id("submit");
+
+    public LoginPage(IWebDriver driver) : base(driver)
+    {
+    }
+
+    public void QaLogin(string username, string password)
+    {
+        driver.Navigate().GoToUrl(URL);
+        EnterText(USERNAME_FIELD, username);
+        EnterText(PASSWORD_FIELD, password);
+        Click(LOGIN_BUTTON);
+    }
+}
